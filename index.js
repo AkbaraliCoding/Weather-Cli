89cd2bbe749dc8d4b82f7d7afda554f060c6c844
akbarali <nematjonovakbarali19@gmail.com>
2023-12-27 01:29:54 -0800

    token

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ import getArgs from './helpers/args.js'
 import { printErr, printSucc, printHelp } from './sevices/log.services.js';
 import { saveKeyValue } from './sevices/storage.services.js';
 
+const saveToken = async token =>{
+    try {
+        await saveKeyValue("Token", token)
+        printSucc('Token Was Saved')
+    } catch (error) {
+        printErr(error.message)
+    }
+}
+
 const startCli = () =>{
     const args = getArgs(process.argv)
-    console.log(args);
     if (args.h) {
         //help Center
         printHelp()
@@ -12,7 +20,7 @@ const startCli = () =>{
     if (args.s) {
         // save sity
     }if (args.t) {
-        saveKeyValue("token", args.t)
+        return saveToken(args.t)
         // save token
     }
 }
